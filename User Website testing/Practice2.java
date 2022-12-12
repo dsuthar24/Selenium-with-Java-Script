1c33a54e5cac3b3a3572159a7d92d085be5f1a35
dsuthar24 <119012169+dsuthar24@users.noreply.github.com>
2022-12-12 15:29:38 -0500

    Website testing q
    User website testing

---
+++ User Website testing/Practice2.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+
+		String text = "Dhruv";
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\14372\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("name")).sendKeys(text);
+		driver.findElement(By.id("alertbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+
+		driver.findElement(By.id("name")).sendKeys(text);
+		driver.findElement(By.id("confirmbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+
+	}
+
+}
